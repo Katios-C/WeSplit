d5dd96a5616df7c9261e2da51cc40ff67282d563
Katios <kate.chonkina@gmail.com>
2020-12-28 09:09:52 +0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Екатерина Чернова on 28.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
