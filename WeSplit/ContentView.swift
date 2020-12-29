a2a971ec1b4b552b372c2c0af2e86277b1f3bc3e
Katios <kate.chonkina@gmail.com>
2020-12-29 13:35:59 +0600

    1

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = "1"
+    @State private var tipPersentage = 2
+    
+    let tipPersentages = [10, 15, 20, 25, 0]
+    
+    
+    var totalPerPerson: Double {
+        
+        let peopleCount = Double(numberOfPeople) ?? 0
+        let tipSelection = Double(tipPersentages[tipPersentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let granTotal = orderAmount + tipValue
+        //var amountPerPerson = 0.0
+        let amountPerPerson = (granTotal / peopleCount)
+       
+        
+        return amountPerPerson
+    }
+    
+    var totalSum: Double {
+        var totalSum = 0.0
+        print(totalPerPerson)
+        totalSum = (totalPerPerson * (Double(numberOfPeople) ?? 0))
+        
+        return totalSum
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            Form {
+                Section {
+                    
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+//                    Picker("Number of people", selection: $numberOfPeople) {
+//                        ForEach(2 ..< 100) {
+//                            Text("\($0) people")
+//                        }
+//                    }
+                    HStack { Section(header: Text("Number of peple:").foregroundColor(.gray)) { TextField("Number of people", text: $numberOfPeople).keyboardType(.decimalPad)
+                    }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave")) {
+                    
+                    Picker("Tip persentage", selection: $tipPersentage) {
+                        ForEach(0 ..< tipPersentages.count) {
+                            Text("\(self.tipPersentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                Section(header: Text("Total sum")) {
+                    Text("Total sum is \(totalSum, specifier: "%.2f")")
+                }
+                }
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
 
@@ -19,3 +83,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
